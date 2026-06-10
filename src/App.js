cfb80899eeb100a65bc54611e446ec88e0a80d33
Cindy Hech <archerychinadiva@gmail.com>
2026-06-10 02:50:44 -0400

    Landing completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import Landing from "./components/Landing";
 import Nav from "./components/Nav";
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
   <div className="App">
     <Nav />
+    <Landing/>
   </div>
   );
 }
